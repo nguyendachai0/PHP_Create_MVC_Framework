afe22539815316c7cfc7a5e746ba7ee709d033fe
Nguyen Dac Hai <nguyendmim@gmail.com>
2023-11-01 20:59:24 +0700

    Add event on Application

--- Application.php
+++ Application.php
@@ -7,6 +7,10 @@ use app\models;
 
 class Application
 {
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
+
+    protected array $eventListeners = [];
     public static string $ROOT_DIR;
 
 
@@ -47,6 +51,7 @@ class Application
     }
     public function run()
     {
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
         try {
 
 
@@ -83,5 +88,18 @@ class Application
     public static function isGuest()
     {
         return !self::$app->user;
+    }
+
+
+    public function on($eventName, $callback)
+    {
+        $this->eventListeners[$eventName][] = $callback;
+    }
+    public function triggerEvent($eventName)
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+        foreach ($callbacks as $callback) {
+            call_user_func($callback);
+        }
     }
 }
